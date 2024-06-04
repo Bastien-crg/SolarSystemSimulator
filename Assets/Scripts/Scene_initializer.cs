319ff4d8dcb76a5ae9608c416c30d88368537a0d
IdrissB77 <59149839+IdrissB77@users.noreply.github.com>
2024-06-04 10:03:37 +0200

    added all planets (+ pluton)

--- Assets/Scripts/Scene_initializer.cs
+++ Assets/Scripts/Scene_initializer.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class Scene_initializer : MonoBehaviour
 {
     public GameObject sun;
-    public GameObject sat;
+    public GameObject mercure;
+    public GameObject venus;
+    public GameObject earth;
+    public GameObject mars;
+    public GameObject jupiter;
+    public GameObject uranus;
+    public GameObject neptune;
+    public GameObject pluton;
     const float G = 66.74f;
+
+    void initiateGravity(GameObject o1, GameObject o2)
+    {
+        Rigidbody rb = o1.GetComponent<Rigidbody>();
+        float R = Vector3.Distance(o1.transform.position, o2.transform.position);
+        float speed = Mathf.Sqrt((G * rb.mass) / R);
+        Debug.Log(speed);
+        Vector3 force = new Vector3(0, 0, 1);
+        force *= speed * 50;
+        Rigidbody rbSat = o2.GetComponent<Rigidbody>();
+        rbSat.AddForce(force);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody rb = sun.GetComponent<Rigidbody>();
-        float R = Vector3.Distance(sun.transform.position, sat.transform.position);
-        float speed = Mathf.Sqrt((G * rb.mass) / R);
-        Debug.Log(speed);
-        Vector3 force = new Vector3(0, 0, 1);
-        force *= speed*50;
-        Rigidbody rbSat = sat.GetComponent<Rigidbody>();
-        rbSat.AddForce(force);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        initiateGravity(sun, pluton);
+        initiateGravity(sun, neptune);
+        initiateGravity(sun, uranus);
+        initiateGravity(sun, jupiter);
+        initiateGravity(sun, mars);
+        initiateGravity(sun, earth);
+        initiateGravity(sun, venus);
+        initiateGravity(sun, mercure);
     }
 }
