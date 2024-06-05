2a275b93d759ff896ca3f0420ef83bd77f127ecc
IdrissB77 <59149839+IdrissB77@users.noreply.github.com>
2024-06-05 09:39:55 +0200

    vrai ss

--- Assets/Scripts/Scene_initializer.cs
+++ Assets/Scripts/Scene_initializer.cs
@@ -8,21 +8,23 @@ public class Scene_initializer : MonoBehaviour
     public GameObject mercure;
     public GameObject venus;
     public GameObject earth;
+    public GameObject moon;
     public GameObject mars;
     public GameObject jupiter;
+    public GameObject saturn;
     public GameObject uranus;
     public GameObject neptune;
     public GameObject pluton;
     const float G = 66.74f;
 
-    void initiateGravity(GameObject o1, GameObject o2)
+    void initiateGravity(GameObject o1, GameObject o2, float speedMult)
     {
         Rigidbody rb = o1.GetComponent<Rigidbody>();
         float R = Vector3.Distance(o1.transform.position, o2.transform.position);
         float speed = Mathf.Sqrt((G * rb.mass) / R);
         Debug.Log(speed);
         Vector3 force = new Vector3(0, 0, 1);
-        force *= speed * 50;
+        force *= speed * speedMult;
         Rigidbody rbSat = o2.GetComponent<Rigidbody>();
         rbSat.AddForce(force);
     }
@@ -30,13 +32,15 @@ public class Scene_initializer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        initiateGravity(sun, pluton);
-        initiateGravity(sun, neptune);
-        initiateGravity(sun, uranus);
-        initiateGravity(sun, jupiter);
-        initiateGravity(sun, mars);
-        initiateGravity(sun, earth);
-        initiateGravity(sun, venus);
-        initiateGravity(sun, mercure);
+        initiateGravity(sun, pluton, 50);
+        initiateGravity(sun, neptune, 50);
+        initiateGravity(sun, uranus, 50);
+        initiateGravity(sun, saturn, 50);
+        initiateGravity(sun, jupiter, 50);
+        initiateGravity(sun, mars, 50);
+        initiateGravity(sun, earth, 50);
+        initiateGravity(earth, moon, 3.5f);
+        initiateGravity(sun, venus, 50);
+        initiateGravity(sun, mercure, 50);
     }
 }
